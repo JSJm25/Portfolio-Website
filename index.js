460840b03ed9ce86006e04573843a116675ac3ef
JSJm25 <jeffreymacmullin@gmail.com>
2025-02-15 15:04:28 -0800

    Added middleware to insert header templates

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
+const { express, app, fs, path, cheerio } = require('./scripts/dependencies.js');
+require('dotenv').config()
 
-const _dir = __dirname + '/views/index.html';
+//Variables
+const _dir = __dirname + '/views/';
+
+//functions
+function loadHtml(filePath)
+{
+    const html = fs.readFileSync(filePath, "utf-8");
+    return cheerio.load(html);
+};
 
 app.use(express.json());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.use((req, res, done) =>{
     let d = new Date();
     console.log(`${req.method}    ${req.path} - ${req.ip} at ${d}`);
     done();
 });
 
+app.use((req, res, next) => {
+    const headerPath = `${__dirname}/views/templates/header.html`;
+    const $header = loadHtml(headerPath);
+    req.headerHTML = $header.html();
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.sendFile(_dir);
-})
+    const path = `${_dir}index.html`
+    const $ = loadHtml(path);
+    $('#header').replaceWith(req.headerHTML);
+    res.send($.html());
+});
+
+app.get('/micros', (req, res) => {
+    const path = `${_dir}micros.html`
+    const $ = loadHtml(path);
+    $('#header').replaceWith(req.headerHTML);
+    res.send($.html());
+});
 
 //Listening Function
 const listener = app.listen(process.env.PORT, () => {
