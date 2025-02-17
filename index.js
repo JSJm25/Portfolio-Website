00d7d4edcbb1243877761dad7dd69f5bf08ff067
JSJm25 <jeffreymacmullin@gmail.com>
2025-02-17 12:37:38 -0800

    moves css into styles folder

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const { express, app, fs, path, cheerio } = require('./scripts/dependencies.js');
+const { express, app, fs, path, cheerio } = require('./modules/dependencies.js');
 
-require('./scripts/micros.js');
-
+require('./modules/micros.js');
 require('dotenv').config()
 
 //Variables
+
 const _dir = __dirname + '/views/';
 
 //functions
+
 function loadHtml(filePath)
 {
     const html = fs.readFileSync(filePath, "utf-8");
     return cheerio.load(html);
 };
+
+//Use Methods
+
 app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -20,20 +24,23 @@ const logger = app.use((req, res, done) =>{
     let d = new Date();
     console.log(`${req.method}    ${req.path} - ${req.ip} at ${d}`);
     done();
-});
+}); // Logs requests
+
 app.use((req, res, next) => {
-    const headerPath = `${__dirname}/views/templates/header.html`;
+    const headerPath = `${_dir}templates/header.html`;
     const $header = loadHtml(headerPath);
     req.headerHTML = $header.html();
     next();
-});
+}); // Use method that that assigns the header element to the request object so It can be appended to any html response
+
+//get methods
 
 app.get('/', (req, res) => {
     const path = `${_dir}index.html`
     const $ = loadHtml(path);
     $('#header').replaceWith(req.headerHTML);
     res.send($.html());
-});
+}); // get request to "/" is seperate from '/:path' because the html file is named index.html and not /.html
 
 app.get('/:path', (req, res) => {
     const p = req.params.path;
@@ -45,6 +52,7 @@ app.get('/:path', (req, res) => {
 
 
 //Listening Function
+
 const listener = app.listen(process.env.PORT, () => {
     console.log(`Listening at http://localhost:${process.env.PORT}/`);
 });
