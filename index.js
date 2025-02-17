35fdd9c3cad8ffd80231de923eee5d95e25d23ce
JSJm25 <jeffreymacmullin@gmail.com>
2025-02-17 11:31:15 -0800

    Added a single get method to navigate the entire site

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { express, app, fs, path, cheerio } = require('./scripts/dependencies.js');
-const { logger } = require('./scripts/utilities.js');
 
 require('./scripts/micros.js');
 
@@ -14,10 +13,14 @@ function loadHtml(filePath)
     const html = fs.readFileSync(filePath, "utf-8");
     return cheerio.load(html);
 };
-
 app.use(express.json());
-app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use('/public', express.static(`${process.cwd()}/public`));
+const logger = app.use((req, res, done) =>{
+    let d = new Date();
+    console.log(`${req.method}    ${req.path} - ${req.ip} at ${d}`);
+    done();
+});
 app.use((req, res, next) => {
     const headerPath = `${__dirname}/views/templates/header.html`;
     const $header = loadHtml(headerPath);
@@ -32,19 +35,14 @@ app.get('/', (req, res) => {
     res.send($.html());
 });
 
-app.get('/micros', (req, res) => {
-    const path = `${_dir}micros.html`
+app.get('/:path', (req, res) => {
+    const p = req.params.path;
+    const path = `${_dir}${p}.html`;
     const $ = loadHtml(path);
     $('#header').replaceWith(req.headerHTML);
     res.send($.html());
-});
+}); //This function alone will navigate most of the site.
 
-app.get('/contact', (req, res) => {
-    const path = `${_dir}contact.html`
-    const $ = loadHtml(path);
-    $('#header').replaceWith(req.headerHTML);
-    res.send($.html());
-});
 
 //Listening Function
 const listener = app.listen(process.env.PORT, () => {
