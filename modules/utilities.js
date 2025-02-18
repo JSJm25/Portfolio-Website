b1c379f1b6d18b78a292560d1d593970cdfba7c5
JSJm25 <jeffreymacmullin@gmail.com>
2025-02-18 15:42:05 -0800

    added loadhtml and pageTitles functions

--- modules/utilities.js
+++ modules/utilities.js
@@ -1,12 +1,32 @@
 const { express, app, fs, path, cheerio } = require('./dependencies.js');
 require('dotenv').config()
 
-const logger = app.use((req, res, done) =>{
-    let d = new Date();
-    console.log(`${req.method}    ${req.path} - ${req.ip} at ${d}`);
-    done();
-});
+function loadHtml(filePath)
+{
+    const html = fs.readFileSync(filePath, "utf-8");
+    return cheerio.load(html);
+};
+
+function pageTitles(string)
+{
+    switch(string)
+    {
+        case 'micros':
+            return "Micro Services"
+        case 'contact':
+            return 'Contact Me'
+        case 'projects':
+            return 'Projects'
+        case 'timestamp':
+            return "TimeStamp Microservice"
+        case 'metadata':
+            return "File MetaData Microservice"
+        default:
+            return "Jeffrey's Portfolio";
+    }
+}
 
 module.exports = {
-    logger
+    loadHtml,
+    pageTitles
 }
