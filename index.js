1e478bfc505ade8e8b4a8a8b95f60c3c2dce753e
JSJm25 <jeffreymacmullin@gmail.com>
2025-02-18 15:37:19 -0800

    Header displays page titles, added get method to navigate dedicated pages for microservices

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { express, app, fs, path, cheerio } = require('./modules/dependencies.js');
+const { loadHtml, pageTitles } = require('./modules/utilities.js');
 
 require('./modules/micros.js');
 require('dotenv').config()
@@ -6,14 +7,6 @@ require('dotenv').config()
 //Variables
 
 const _dir = __dirname + '/views/';
-
-//functions
-
-function loadHtml(filePath)
-{
-    const html = fs.readFileSync(filePath, "utf-8");
-    return cheerio.load(html);
-};
 
 //Use Methods
 
@@ -26,15 +19,15 @@ const logger = app.use((req, res, done) =>{
     done();
 }); // Logs requests
 
+ // Use method that that assigns the header element to the request object so It can be appended to any html response
 app.use((req, res, next) => {
     const headerPath = `${_dir}templates/header.html`;
     const $header = loadHtml(headerPath);
     req.headerHTML = $header.html();
     next();
-}); // Use method that that assigns the header element to the request object so It can be appended to any html response
+});
 
 //get methods
-
 app.get('/', (req, res) => {
     const path = `${_dir}index.html`
     const $ = loadHtml(path);
@@ -42,14 +35,32 @@ app.get('/', (req, res) => {
     res.send($.html());
 }); // get request to "/" is seperate from '/:path' because the html file is named index.html and not /.html
 
+
 app.get('/:path', (req, res) => {
     const p = req.params.path;
     const path = `${_dir}${p}.html`;
     const $ = loadHtml(path);
     $('#header').replaceWith(req.headerHTML);
+    const titleString = pageTitles(p);
+    $("#page-title").text(titleString);
     res.send($.html());
-}); //This function alone will navigate most of the site.
+}); //Navigate the main pages on the site
 
+app.get('/micros/:path', (req, res) => {
+        const p = req.params.path;
+        const path = `${_dir}/microservices/${p}.html`;
+        const $ = loadHtml(path);
+        const microsPath = `${_dir}/micros.html`
+        const $microservicecollection = loadHtml(microsPath);
+        const microServiceHTMLDiv = $microservicecollection(`#${p}`) 
+        $('#header').replaceWith(req.headerHTML);
+        $(`#${p}`).replaceWith(microServiceHTMLDiv);
+
+        const titleString = pageTitles(p);
+        $("#page-title").text(titleString);
+
+        res.send($.html());
+}); //Microservices Navigation
 
 //Listening Function
 
